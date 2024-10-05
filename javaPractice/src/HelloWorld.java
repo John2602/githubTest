01011ebb19d33bb70800b4c3a4af16030d794aab
John2607 <johnkarthik017@gmail.com>
2024-10-05 12:44:21 +0530

    First commit

---
+++ javaPractice/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    String fistName;
+    String lastName;
+
+    public  HelloWorld(String fistName, String lastName) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return fistName + " " + lastName;
+    }
+
+
+}
